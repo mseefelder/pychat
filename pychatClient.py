98bf459a9b6ede1a6bf08fca183a3d9d40b4ec63
mseefelder <saa.marcos@gmail.com>
2015-06-29 14:57:04 -0300

    First commit, pychat initial version, with some fixes in relation to the tutorial referenced in our description.

---
+++ pychatClient.py
@@ -0,0 +1,53 @@
+# telnet program example
+import socket, select, string, sys
+ 
+def prompt() :
+    sys.stdout.write('<You> ')
+    sys.stdout.flush()
+ 
+#main function
+if __name__ == "__main__":
+     
+    if(len(sys.argv) < 3) :
+        print 'Uso : python pychatClient.py host porta \n Example: python pychatClient.py localhost 5000'
+        sys.exit()
+     
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+     
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+     
+    # connect to remote host
+    try :
+        s.connect((host, port))
+    except :
+        print 'Falha ao conectar com o servidor'
+        sys.exit()
+     
+    print 'Conectado. Pode mandar mensagens...'
+    prompt()
+     
+    while True:
+        socket_list = [sys.stdin, s]
+         
+        # Get the list sockets which are readable
+        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+         
+        for sock in read_sockets:
+            #incoming message from remote server
+            if sock == s:
+                data = sock.recv(4096)
+                if not data :
+                    print '\nDesconectado.'
+                    sys.exit()
+                else :
+                    #print data
+                    sys.stdout.write(data)
+                    prompt()
+             
+            #user entered a message
+            else :
+                msg = sys.stdin.readline()
+                s.send(msg)
+                prompt()
