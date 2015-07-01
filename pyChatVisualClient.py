42b9aee914f1c7959c5190b21886fa22accdc70a
Lucas-CG <lucas.gomes@poli.ufrj.br>
2015-06-30 22:05:12 -0300

    Incluídas threads e reações a erros de socket. Código ainda não funciona.

--- pyChatVisualClient.py
+++ pyChatVisualClient.py
@@ -1,46 +1,96 @@
 from curses import wrapper
 from ui import ChatUI
 import socket, select, string, sys
-import thread
+from threading import Thread
+from time import sleep
 
 def receptor(serverSocket, ui):
 	while 1:
-		msg = serverSocket.recv(4096)
-		if not msg: break
-		ui.chatbuffer_add(msg)
+		#msg = serverSocket.recv(4096)
+		#if not msg: break
+		#ui.chatbuffer_add(msg)
+		try:
+			msg = serverSocket.recv(4096)
+		except socket.timeout, e:
+			err = e.args[0]
+			if err == 'timed out':
+				sleep(1)
+				print 'recv timed out, retry later'
+				continue
+			else:
+				print e
+				sys.exit(1)
+		except socket.error, e:
+			print e
+			sys.exit(1)
+		else:
+			#if len(msg) == 0;
+			if not msg:
+				#print 'orderly shutdown on server end'
+				#sys.exit(0)
+				break
+			else:
+				ui.chatbuffer_add(msg)
 
-def emissor(serverSocket):
+def emissor(serverSocket, ui):
 	msg = ""
-    while msg != "/quit":
-        msg = ui.wait_input()
-    serverSocket.send(msg)
+	while msg != "/quit":
+		msg = ui.wait_input()
+		serverSocket.send(msg)
 
 def main(stdscr):
-    stdscr.clear()
-    ui = ChatUI(stdscr)
-    host = ui.wait_input("Host: ")
-    port = ui.wait_input("Porta: ")
-    username = ui.wait_input("Apelido: ")
+	stdscr.clear()
+	ui = ChatUI(stdscr)
+	host = ui.wait_input("Host: ")
+	port = int(ui.wait_input("Porta: "))
+	username = ui.wait_input("Apelido: ")
+	ui.userlist.append(username)
+	ui.redraw_userlist()
 
-    #Connection setup
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
+	#Connection setup
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.settimeout(2)
      
-    # connect to remote host
-    try :
-        s.connect((host, port))
-    except :
-        print 'Falha ao conectar com o servidor'
-        sys.exit()
+	# connect to remote host
+	try :
+		s.connect((host, port))
+	except :
+		ui.chatbuffer_add('Falha ao conectar com o servidor')
+		sys.exit()
 
-    #Regisra o usuario:
-    s.send("setUsername->"+username)
+	#Registra o usuario:
+	s.send("setUsername->"+username)
 
-    ui.chatbuffer_add('Conectado. Pode mandar mensagens...\n')
+	ui.chatbuffer_add('Conectado. Pode mandar mensagens...\n')
+
+
+	#Thread do Emissor
+
+	class TEmissor(Thread):
+		def __init__ (self):
+			Thread.__init__(self)
+
+		def run (self):
+			emissor(s, ui)
+
+	class TReceptor(Thread):
+		def __init__ (self):
+			Thread.__init__(self)
+
+		def run (self):
+			receptor(s, ui)
 
     #TO DO: Chamar as duas threads aqui:
-    thread.start_new_thread(receptor, (s, ui))
-    thread.start_new_thread(emissor, (s))
+
+	em = TEmissor()
+	re = TReceptor()
+	em.start()
+	re.start()
+
+   
+    #thread.start_new_thread(receptor, (s, ui))
+    #thread.start_new_thread(emissor, (s))
+
     #TALVEZ tenha que usar locks: serverSocket.recv(4096) no receptor trava 
     #enquanto nao recebe nada do servidor.
     #a boa eh colocar um timeout e lock.aquire() espera mensagem por
