c6f837796047e59365582c09fa25b5c3bd925cb7
mseefelder <saa.marcos@gmail.com>
2015-07-01 00:52:26 -0300

    Working server and client for talk application

---
+++ talkClient.py
@@ -0,0 +1,78 @@
+from curses import wrapper
+from ui import ChatUI
+import socket, select, string, sys
+import thread
+import time
+
+def emissor(serverSocket,ui,lock):
+	print "Emissor"
+	ui.chatbuffer_add("Receiving")
+	while True:
+		try:
+			msg = serverSocket.recv(4096)
+		except socket.timeout, e:
+			err = e.args[0]
+			# this next if/else is a bit redundant, but illustrates how the
+			# timeout exception is setup
+			if err == 'timed out':
+				#time.sleep(1)
+				#print 'recv timed out, retry later'
+				continue
+			else:
+				print e
+				ui.chatbuffer_add(err)
+				sys.exit(1)
+		except socket.error, e:
+			# Something else happened, handle error, exit, etc.
+			print e
+			ui.chatbuffer_add(e.args[0])
+			sys.exit(1)
+		else:
+			if len(msg) == 0:
+				print 'orderly shutdown on server end'
+				sys.exit(0)
+			else:
+				ui.chatbuffer_add("<Interlocutor> "+msg)
+
+def receptor(serverSocket,ui,lock):
+	ui.chatbuffer_add("Chatting")
+	inp = ""
+	while inp != "/quit":
+		inp = ui.wait_input()
+		serverSocket.send(inp+"\n")
+
+def main(stdscr):
+	stdscr.clear()
+	ui = ChatUI(stdscr)
+
+	host = ui.wait_input("Host: ")
+	port = int(ui.wait_input("Porta: "))
+	username = ui.wait_input("Apelido: ")
+
+	#Connection setup
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.settimeout(5)
+
+    # connect to remote host
+	try :
+		s.connect((host, port))
+	except :
+		print 'Falha ao conectar com o servidor'
+		sys.exit()
+
+	ui.chatbuffer_add('Conectado. Pode mandar mensagens...\n')
+
+	ui.redraw_ui()
+
+	lock = thread.allocate_lock()
+	thread.start_new_thread(emissor, (s, ui,lock))
+	#thread.start_new_thread(receptor, (s, ui,lock))
+
+	inp = ""
+	while inp != "/quit":
+		inp = ui.wait_input()
+		ui.chatbuffer_add("<Eu> "+inp)
+		s.send(inp+" \n")
+
+if __name__ == "__main__":
+	wrapper(main)
---
+++ talkServer.py
@@ -0,0 +1,67 @@
+from curses import wrapper
+from ui import ChatUI
+import socket, select, string, sys
+import thread
+import time
+
+def receptor(listenerSocket,ui,lock):
+	ui.chatbuffer_add("Recebendo.")
+	while True:
+		try:
+			msg = listenerSocket.recv(4096)
+		except socket.timeout, e:
+			err = e.args[0]
+			# this next if/else is a bit redundant, but illustrates how the
+			# timeout exception is setup
+			if err == 'timed out':
+				#time.sleep(1)
+				#print 'recv timed out, retry later'
+				continue
+			else:
+				print e
+				ui.chatbuffer_add(err)
+				sys.exit(1)
+		except socket.error, e:
+			# Something else happened, handle error, exit, etc.
+			print e
+			ui.chatbuffer_add(e.args[0])
+			sys.exit(1)
+		else:
+			if len(msg) == 0:
+				print 'orderly shutdown on server end'
+				sys.exit(0)
+			else:
+				ui.chatbuffer_add("<Interlocutor> " + msg)
+
+def main(stdscr):
+	stdscr.clear()
+	ui = ChatUI(stdscr)
+
+	RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
+	PORT = 5000
+     
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # this has no effect, why ?
+	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	server_socket.bind(("0.0.0.0", PORT))
+	server_socket.listen(1)
+
+	ui.chatbuffer_add('Aguardando ouvinte...\n')
+
+	listener, addr = server_socket.accept()
+
+	ui.chatbuffer_add('Conectado. Pode mandar mensagens...\n')
+
+	ui.redraw_ui()
+
+	lock = thread.allocate_lock()
+	thread.start_new_thread(receptor, (listener, ui, lock))
+
+	inp = ""
+	while inp != "/quit":
+		inp = ui.wait_input()
+		ui.chatbuffer_add("<Eu> "+inp)
+		listener.send(inp+" \n")
+
+if __name__ == "__main__":
+	wrapper(main)
